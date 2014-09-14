b42b3c51a83ebb523a6a7d2c109535b23d4b5b49
mrpc <mrpc@pramnoshosting.gr>
2014-09-14 21:28:46 +0300

    Added htop

--- _build/vagrant/bootstrap.sh
+++ _build/vagrant/bootstrap.sh
@@ -27,7 +27,7 @@ yes | add-apt-repository ppa:ondrej/php5
 apt-get update
 
 # Install requirements
-apt-get install -y apache2 build-essential checkinstall php5 php5-cli php5-mcrypt php5-gd php-apc git sqlite php5-sqlite curl php5-curl php5-dev php-pear php5-xdebug vim-nox ruby rubygems sqlite3 libsqlite3-dev memcached php5-memcache
+apt-get install -y apache2 build-essential checkinstall php5 php5-cli php5-mcrypt php5-gd php-apc git sqlite php5-sqlite curl php5-curl php5-dev php-pear php5-xdebug vim-nox ruby rubygems sqlite3 libsqlite3-dev memcached php5-memcache htop
 
 
 # Install MySQL
